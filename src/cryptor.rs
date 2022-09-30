a1c09fe1d5baee3900a0372c1d9cd65c07503202
Alex Gievsky <alex.gievsky@gmail.com>
2022-09-30 10:41:17 +0200

    Introduce cryptor.encrypt

--- src/cryptor.rs
+++ src/cryptor.rs
@@ -1,7 +1,7 @@
 use std::{sync::Arc};
 
 use prost::encoding::bool;
-use crate::{prekey::Prekey, session::{Session, self}, mac::AxolotlMac, serializable::Deserializable, x448::KeyPairX448, ntru::{KeyPairNtru, NtruedKeys, self, PrivateKeyNtru, DecryptionMode::Double}, signed_key_pair::SignedKeyPair, message::Type};
+use crate::{prekey::Prekey, session::{Session, self}, mac::AxolotlMac, serializable::{Deserializable, Serializable}, x448::{KeyPairX448, PublicKeyX448}, ntru::{KeyPairNtru, NtruedKeys, self, PrivateKeyNtru, DecryptionMode::Double, PublicKeyNtru}, signed_key_pair::SignedKeyPair, message::Type, ed448::{PublicKeyEd448, KeyPairEd448}, signed_public_key::SignedPublicKeyX448};
 
 /*
 
@@ -12,43 +12,48 @@ Session: { id(primary), nid, blob, receive_only, restoring }
 
 */
 
-pub trait Sessions {
+pub trait Storage {
 	/// Should ignore receive_only sessions
 	// TODO: replace with `Nid`; should exclude receive_only session
-	fn get_active(&self, nid: &str) -> Option<Session>;
+	// TODO: should be result to include the "DB is locked" case
+	fn get_active_session_for_nid(&self, nid: &str) -> Option<Session>;
 	/// Returns any session, whether active or receive_only
-	fn get_by_id(&self, id: u64) -> Option<Session>;
+	fn get_session_by_id(&self, id: u64) -> Option<Session>;
 
 	/// Clears active and receive_only sessions, if any
-	fn clear_all(&self, nid: &str); // TODO: result?
-	fn save(&self, session: &Session, nid: &str, id: u64, receive_only: bool); // TODO: introduce result
-}
+	fn clear_all_sessions_for_nid(&self, nid: &str); // TODO: result?
+	fn save_session(&self, session: &Session, nid: &str, id: u64, receive_only: bool); // TODO: introduce result
 
-pub trait Identities {
+	// Identity
 	fn get_my_identity(&self) -> Option<KeyPairX448>; // TODO: Result with a custom error type?
+	fn get_my_signing_identity(&self) -> Option<KeyPairEd448>;
 	fn get_my_ntru_identity(&self) -> Option<KeyPairNtru>; // TODO: result with a custom error type?
+	
+	fn get_identity_for_nid(&self, nid: &str) -> Option<IdentityKeys>;
+	fn save_identity(&self, identity: &IdentityKeys, nid: &str);
+
+	// Prekeys
+	fn get_prekey(&self, id: u64) -> Option<Prekey>; // TODO: use Result instead; a separate consume?
+	fn get_signed_prekey(&self, id: u64) -> Option<SignedKeyPair>; // TODO: use Result instead and handle errors: locked vs not found
+
+	/// deletes a Prekey where id = prekey.x448_key.id, if any
+	fn delete_prekey(&self, id: u64);
 }
 
-// TODO: should this be async actually?
-pub trait Prekeys {
-	fn get(&self, id: u64) -> Option<Prekey>; // TODO: use Result instead; a separate consume?
-	fn get_signed(&self, id: u64) -> Option<SignedKeyPair>; // TODO: use Result instead and handle errors: locked vs not found
-	/// deletes a Prekey where id = prekey.x448_key.id, if any
-	fn delete(&self, id: u64);
+pub trait Apis {
+	fn fetch_prekey(&self, nid: &str) -> Result<FetchedPrekeyBundle, Error>;
 }
 
 pub struct Cryptor {
-	sessions: Arc<dyn Sessions>,
-	prekeys: Arc<dyn Prekeys>,
-	identities: Arc<dyn Identities>
+	storage: Arc<dyn Storage>,
+	apis: Arc<dyn Apis>
 }
 
 impl Cryptor {
-	pub fn new(sessions: Arc<dyn Sessions>, prekeys: Arc<dyn Prekeys>, identities: Arc<dyn Identities>) -> Self {
+	pub fn new(storage: Arc<dyn Storage>, apis: Arc<dyn Apis>) -> Self {
 		Self {
-			sessions: Arc::clone(&sessions),
-			prekeys: Arc::clone(&prekeys),
-			identities: Arc::clone(&identities)
+			storage: Arc::clone(&storage),
+			apis: Arc::clone(&apis)
 		}
 	}
 }
@@ -60,6 +65,10 @@ pub enum Error {
 	NoIdentityFound,
 	/// DB is locked/corrupted/not ready; try again later
 	NoNtruIdentityFound,
+	/// DB is locked/corrupted/not ready; try again later
+	NoSigningIdentityFound,
+	/// Previously saved identity does not match with the backend's response; reset?
+	IdentityDoesNotMatch,
 	/// A stale signed key is used; reset
 	NoSignedPrekeyFound(u64),
 	/// A prekey has already been used by someone else (quite impossible) or there was a crash previously; reset
@@ -78,6 +87,21 @@ pub struct Decrypted {
 	_type: Type
 }
 
+// TODO: rename
+pub struct IdentityKeys {
+	x448: PublicKeyX448,
+	ntru: PublicKeyNtru,
+	ed448: PublicKeyEd448
+}
+
+// TODO: rename
+pub struct FetchedPrekeyBundle {
+	prekey_x448: PublicKeyX448,
+	prekey_ntru: PublicKeyNtru,
+	signed_prekey_x448: SignedPublicKeyX448,
+	identity: IdentityKeys
+}
+
 impl Cryptor {
 	pub async fn decrypt(&mut self, mac: &[u8], nid: &str, my_nid: &str) -> Result<Decrypted, Error> {
 		// all the state change should be saved here, not by the caller – should it?
@@ -86,14 +110,14 @@ impl Cryptor {
 		// a new session is being initiated (doesn't mean it's the first message though)
 		if let Some(ref kex) = mac.body().key_exchange {
 			// this can be both, active and receive_only session – does not matter at this point
-			if let Some(session) = self.sessions.get_by_id(kex.id()) {
+			if let Some(session) = self.storage.get_session_by_id(kex.id()) {
 				return self.decrypt_with_session(session, mac, nid);
 			} else {
-				let identity = self.identities.get_my_identity().ok_or(Error::NoIdentityFound)?;
-				let ntru_identity = self.identities.get_my_ntru_identity().ok_or(Error::NoNtruIdentityFound)?;
+				let identity = self.storage.get_my_identity().ok_or(Error::NoIdentityFound)?;
+				let ntru_identity = self.storage.get_my_ntru_identity().ok_or(Error::NoNtruIdentityFound)?;
 
-				let signed_prekey = self.prekeys.get_signed(kex.signed_prekey_id).ok_or(Error::NoSignedPrekeyFound(kex.signed_prekey_id))?;
-				let Prekey { key_x448, key_ntru, .. } = self.prekeys.get(kex.x448_prekey_id).ok_or(Error::NoPrekeyFound(kex.x448_prekey_id))?;
+				let signed_prekey = self.storage.get_signed_prekey(kex.signed_prekey_id).ok_or(Error::NoSignedPrekeyFound(kex.signed_prekey_id))?;
+				let Prekey { key_x448, key_ntru, .. } = self.storage.get_prekey(kex.x448_prekey_id).ok_or(Error::NoPrekeyFound(kex.x448_prekey_id))?;
 				let find_key = |_| -> Result<&PrivateKeyNtru, ntru::Error> {
 					Ok(key_ntru.private_key())
 				};
@@ -110,12 +134,12 @@ impl Cryptor {
 
 				// TODO: check current.has_receive only first? –if yes, clear as well
 				if kex.force_reset {
-					self.sessions.clear_all(nid);
+					self.storage.clear_all_sessions_for_nid(nid);
 
 					return self.decrypt_with_session(session, mac, nid);
 				} else {
 					// do I have any other session for this nid?
-					if let Some(_) = self.sessions.get_active(nid) {
+					if let Some(_) = self.storage.get_active_session_for_nid(nid) {
 						if session.role() == session::Role::Alice {
 							if Self::should_be_alice(my_nid, nid) {
 								// the sender is considering herself Alice (but they'll fix themselves eventually), so keep 
@@ -126,13 +150,13 @@ impl Cryptor {
 							} else {
 								// I was Alice, but at the same time some one initiated a session and I actually should be Bob
 								// now, I'll delete my session and will use the new one
-								self.sessions.clear_all(nid);
+								self.storage.clear_all_sessions_for_nid(nid);
 
 								return self.decrypt_with_session(session, mac, nid);
 							}
 						} else {
 							// I'm bob already, but from now on, I should be using this new session only
-							self.sessions.clear_all(nid);
+							self.storage.clear_all_sessions_for_nid(nid);
 
 							return self.decrypt_with_session(session, mac, nid);
 						}
@@ -143,7 +167,7 @@ impl Cryptor {
 				}
 			}
 		} else {
-			if let Some(current) = self.sessions.get_active(nid) {
+			if let Some(current) = self.storage.get_active_session_for_nid(nid) {
 				// at this point, it could be save to delete any receive_only sessions, if any
 				return self.decrypt_with_session(current, mac, nid);
 			} else {
@@ -159,54 +183,75 @@ impl Cryptor {
 
 	fn decrypt_with_session(&self, mut session: Session, mac: AxolotlMac, nid: &str) -> Result<Decrypted, Error> {
 		if let Ok(msg) = session.decrypt(&mac) {
-			// TODO: respect receive_only
-			self.sessions.save(&session, nid, session.id(), session.receive_only());
+			self.storage.save_session(&session, nid, session.id(), session.receive_only());
 
 			// TODO: session itself could keep Option<prekey_id> and clear it per each decryption, if required
 			if let Some(id) = mac.body().key_exchange.as_ref().and_then(|k| Some(k.x448_prekey_id)) {
-				self.prekeys.delete(id);
+				self.storage.delete_prekey(id);
 			}
 
 			Ok(Decrypted { msg, _type: mac.body()._type })
 		} else {
-			self.sessions.clear_all(nid);
+			self.storage.clear_all_sessions_for_nid(nid);
 
 			Err(Error::WrongMac)
 		}
 	}
 
-	// TODO: force ntru?
-	// TODO: force reset?
-	// TODO: MessageType?
-	pub async fn encrypt(&self, plaintext: &[u8], _type: Type, nid: &str, force_reset: bool) -> Vec<u8> {
-		// TODO: if force_reset, create a new session?
+	// force_ntru, as is now implemented, is not what it might look like: it can ntru-encrypt my next ratchet when the time
+	// to turn comes, but it can't turn it emmidiately because of Axolotl's strict ping-pong nature
+	pub async fn encrypt(&self, plaintext: &[u8], _type: Type, nid: &str, force_reset: bool) -> Result<Vec<u8>, Error> {
+		if force_reset {
+			self.storage.clear_all_sessions_for_nid(nid);
+		}
 
-		// this ignores receive_only sessions
-		if let Some(current) = self.sessions.get_active(nid) {
+		if let Some(current) = self.storage.get_active_session_for_nid(nid) {
 			return self.encrypt_with_session(current, plaintext, _type, nid);
 		} else {
+			let my_identity = self.storage.get_my_identity().ok_or(Error::NoIdentityFound)?;
+			let my_ntru_identity = self.storage.get_my_ntru_identity().ok_or(Error::NoNtruIdentityFound)?;
+			let my_signing_identity = self.storage.get_my_signing_identity().ok_or(Error::NoSigningIdentityFound)?;
+			let my_ratchet = KeyPairX448::generate();
+			let my_ntru_ratchet = KeyPairNtru::generate();
+			let bundle = self.apis.fetch_prekey(nid)?; // TODO: respect UserDoesNotExist + network errors
+
+			if let Some(identity) = self.storage.get_identity_for_nid(nid) {
+				if identity.x448 != bundle.identity.x448 || identity.ntru != bundle.identity.ntru || identity.ed448 != bundle.identity.ed448 {
+					return Err(Error::IdentityDoesNotMatch);
+				}
+			} else {
+				self.storage.save_identity(&bundle.identity, nid);
+			}
+
+			let session = Session::alice(my_identity,
+				my_ratchet,
+				my_signing_identity,
+				my_ntru_identity,
+				my_ntru_ratchet, 
+				bundle.identity.x448,
+				bundle.signed_prekey_x448,
+				bundle.prekey_x448,
+				bundle.prekey_ntru,
+				bundle.identity.ntru,
+				force_reset);
+
+				return self.encrypt_with_session(session, plaintext, _type, nid);
 		}
-		// 1 get active_session
-		// 2 create a new one, if not found:
-		//  2.1 read recipient identity
-		//  2.2 fetch prekey – respect UserDoesNotExist as well as network errors
-		// 		{ { x448, ntru }, x448, ntru, signed_x448, ed448 }
-		//	2.2' read peer identity? { x448, ntru, ed48 }?
-		//  2.3 create Alice
-		// 	2.3' save peer identity?
-		//  2.4 save Alice?
-		// 3 encrypt: if failed, clear
-		// 4 save
-		// 5 encode
-		// return
-		todo!()
 	}
 
-	fn encrypt_with_session(&self, session: Session, plaintext: &[u8],_type: Type, nid: &str) -> Vec<u8> {
-		todo!()
+	// add_prekey
+	// add_signed_prekey
+	// is_signed_prekey_stale
+
+	fn encrypt_with_session(&self, mut session: Session, plaintext: &[u8], _type: Type, nid: &str) -> Result<Vec<u8>, Error> {
+		let ciphertext = session.encrypt(plaintext, _type);
+
+		self.storage.save_session(&session, nid, session.id(), false);
+		// Desktop keeps restarting indefinitely if encrypt throws, but can it can't fail now
+
+		return Ok(ciphertext.serialize());
 	}
 }
-
 
 #[cfg(test)]
 mod tests {
